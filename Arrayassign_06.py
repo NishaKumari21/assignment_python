8412512aa2b40f0cc34506f9c6754c44597240db
Nisha Kumari <129987725+NishaKumari21@users.noreply.github.com>
2025-01-19 20:05:48 +0530

    Update Arrayassign_06.py

--- Arrayassign_06.py
+++ Arrayassign_06.py
@@ -1,6 +1,6 @@
 # 1. Write a Python Program to find sum of array?
 # 2. Write a Python Program to find largest element in an array?
-# 3. Write a Python Program for array rotation?
+# 3. Write a Python Program for checking the array length?
 # 4. Write a Python Program to Split the array and add the first part to the end?
 # 5. Write a Python Program to check if given array is Monotonic?
 # Q1.To print of array:
