57bb9f933589ab763fb064ac6feb198c6d6f733b
Nisha Kumari <129987725+NishaKumari21@users.noreply.github.com>
2025-01-21 23:35:53 +0530

    Add files via upload
    This file is about the decorators used in python.

---
+++ decorator_11.py
@@ -0,0 +1,24 @@
+# decorator are usef to add more functionality and features to the existing function
+# for eg it denominator is greater then numinator then error will comes so will can change the format of fucntion without doing anything to code just by adding another function 
+# here decorator means no matter in which order we pass the function argmnets but we want the answer same and everytime the b should be greater ie denominator
+# def div(a,b):
+#     if a<b:
+#         a,b=b,a
+#     print(a/b)
+# div(2,4)
+# 2.0
+# we can do likhe this also but we can use decorator and make this work properly like this without doing anything inside code
+
+def div(a,b):
+   return a/b
+def smart_div(func):
+    def inner(a,b):
+     if a<b:
+        a,b=b,a
+        return func(a,b)
+    return inner
+div1=smart_div(div)
+print(div1(2,4))
+# 2.0
+# 0.5
+print(div(2,4))
